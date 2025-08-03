2fd16c72548fdd060beaa26941df3c9d6c0fa80b
재민 <jjm0203311@naver.com>
2025-08-04 06:56:08 +0900

    메인 클래스 설정

--- backend/src/main/java/org/likelionhsu/backend/BackendApplication.java
+++ backend/src/main/java/org/likelionhsu/backend/BackendApplication.java
@@ -1,13 +1,28 @@
 package org.likelionhsu.backend;
 
+import org.likelionhsu.backend.common.config.KmaApiProperties;
+import org.likelionhsu.backend.common.config.KmaApiProperties;
+import org.likelionhsu.backend.crawler.config.NaverSearchProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties({NaverSearchProperties.class, KmaApiProperties.class})
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
+
+
