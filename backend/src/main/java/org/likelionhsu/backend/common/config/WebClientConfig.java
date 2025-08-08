9f3643ac80740a3343f5817d19b0cc94a059c411
재민 <jjm0203311@naver.com>
2025-08-08 21:32:43 +0900

    config 수정

--- backend/src/main/java/org/likelionhsu/backend/common/config/WebClientConfig.java
+++ backend/src/main/java/org/likelionhsu/backend/common/config/WebClientConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+    public WebClient webClient(WebClient.Builder builder, @Value("${crawler.api.url:http://crawler:5001}") String crawlerBaseUrl) {
+        return builder.baseUrl(crawlerBaseUrl).build();
     }
 }
